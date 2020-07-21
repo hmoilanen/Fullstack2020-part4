0d4e9eca42af7e4591d6c8d98367b1c57d1ac827
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-21 15:05:11 +0300

    part 4.1 - done

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-/* const http = require('http')
+// Fullstack2020 - Part 4a
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
-})
-
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`) */
-
-
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -25,11 +16,22 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+//const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -37,6 +39,7 @@ app.get('/api/blogs', (request, response) => {
     .then(blogs => {
       response.json(blogs)
     })
+    .catch(error => console.log(error))
 })
 
 app.post('/api/blogs', (request, response) => {
@@ -47,9 +50,10 @@ app.post('/api/blogs', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
+    .catch(error => console.log(error))
 })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
