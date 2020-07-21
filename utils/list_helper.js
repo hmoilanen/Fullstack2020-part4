9b5c0eaf833cda2c4f984da74fdef5a51eaa314f
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-21 19:26:31 +0300

    part 4.4 - done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,19 @@
 const dummy = (blogs) => {
-	console.log('blogs:', blogs)
+	//console.log('dummy - blogs:', blogs)
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	//console.log('totalLikes - blogs:', blogs)
+	const likes = blogs.map(blog => blog.likes)
+	const sumOfLikes = (sum, added) => {
+		return sum + added
+	}
+
+	return likes.reduce(sumOfLikes, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
