7b494df970f59cf6633f95bb57e74f4b8448080f
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-21 16:48:06 +0300

    part 4.3 - done

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const server = http.createServer(app)
 
 const PORT = config.PORT || 3003
 server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+	logger.info(`Server running on port ${PORT}`)
 })
