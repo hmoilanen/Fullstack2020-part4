f42efa25bd139b16c96ea3e9aafd68cbecebb3fb
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-07-27 20:33:22 +0300

    part 4.12 - done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -54,11 +54,38 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-	try {
-		await Blog.findByIdAndRemove(request.params.id)
-		response.status(204).end()
-	} catch (exception) {
-		next(exception)
+	const token = request.token
+	const decodedToken = !token
+		? false
+		: jwt.verify(token, process.env.SECRET)
+
+	if (!token || !decodedToken.id) {
+		return response.status(401).json({ error: 'token missing or invalid' })
+	}
+		
+	let blogToDelete = await Blog.findById(request.params.id)
+
+	if (decodedToken.id === blogToDelete.user.toString()) {
+		try {
+			await Blog.findByIdAndRemove(blogToDelete._id.toString())
+			response.status(204).json({ message: 'blog deleted successfully' })
+		} catch (exception) {
+			next(exception)
+		}
+		
+		let authorOfBlog = await User.findById(decodedToken.id)
+		authorOfBlog.blogs = authorOfBlog.blogs.filter(blog => {
+			return blog.toString() !== blogToDelete._id.toString()
+		})
+
+		try {
+			await authorOfBlog.save()
+			response.status(201).end()
+		} catch (exception) {
+			next(exception)
+		}
+	} else {
+		response.status(401).json({ error: 'deleting this blog in unauthorized' })
 	}
 })
 
